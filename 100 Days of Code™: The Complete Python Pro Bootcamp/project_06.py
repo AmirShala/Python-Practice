9ae21825e11aa97d3117769508f847f68681c4ef
Amir <96538080+AmirShala@users.noreply.github.com>
2026-03-19 15:15:46 +0200

    Create project_06.py

---
+++ 100 Days of Code™: The Complete Python Pro Bootcamp/project_06.py
@@ -0,0 +1,20 @@
+The following code will solve "The Maze" problem in Reeborg's World 
+URL = (https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json)
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while not at_goal():
+    if right_is_clear():
+        if front_is_clear():
+            move()
+        else:
+            turn_right()
+            move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
