fef6a0d177cdf986a51c7aacdbf7775296cd4a4e
Amir <96538080+AmirShala@users.noreply.github.com>
2026-03-19 15:16:27 +0200

    Update project_06.py

--- 100 Days of Code™: The Complete Python Pro Bootcamp/project_06.py
+++ 100 Days of Code™: The Complete Python Pro Bootcamp/project_06.py
@@ -1,5 +1,5 @@
-The following code will solve "The Maze" problem in Reeborg's World 
-URL = (https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json)
+'''The following code will solve "The Maze" problem in Reeborg's World 
+URL = (https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json)'''
 
 def turn_right():
     turn_left()
